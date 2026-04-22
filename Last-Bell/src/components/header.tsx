b3eb3efb3b268a26ec1a652ff1f7462adb786fca
taeyeon <greenjuni7@naver.com>
2026-04-22 16:58:37 +0900

    feat:: header 퍼블리싱

---
+++ Last-Bell/src/components/header.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import { Colors } from "../styles/color";
+import { FaBell } from "react-icons/fa";
+
+interface HeaderProps {
+  title: string;
+}
+
+export const Header = ({ title }: HeaderProps) => {
+  return (
+    <>
+      <Wrapper>
+        <Title>{title}</Title>
+        <Right>
+          <Bell>
+            <FaBell />
+          </Bell>
+          <ProfileWrapper>
+            <ProfileImg></ProfileImg>
+            <ProfileName>이태연</ProfileName>
+          </ProfileWrapper>
+        </Right>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: ${Colors.Black};
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+`;
+
+const Bell = styled.div`
+  margin-top: 8px;
+  transform: scale(1.3);
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+`;
+
+const ProfileImg = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background-color: ${Colors.Blue400};
+`;
+
+const ProfileName = styled.div`
+  font-size: 16px;
+`;
+// n>0
+// n%2
