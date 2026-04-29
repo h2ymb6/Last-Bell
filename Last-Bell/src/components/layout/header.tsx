9dbfce6dffcb05bc67f074929d0e681681974388
taeyeon <greenjuni7@naver.com>
2026-04-29 14:33:48 +0900

    fix:: 경로변경

--- Last-Bell/src/components/layout/header.tsx
+++ Last-Bell/src/components/layout/header.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Colors } from "../styles/color";
+import { Colors } from "../../styles/color";
 import { FaBell } from "react-icons/fa";
 
 interface HeaderProps {
