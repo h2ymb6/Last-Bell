6e0cd6a23d5594afadab891f9cb7239a51150842
taeyeon <greenjuni7@naver.com>
2026-05-27 09:14:45 +0900

    feat:: router

--- Last-Bell/src/App.tsx
+++ Last-Bell/src/App.tsx
@@ -1,10 +1,27 @@
 import DashBoard from "./pages/dashboard";
 import ExamAnd from "./pages/examAND";
+import SchoolSchedule from "./pages/schoolSchedule";
+import FloorMap from "./pages/FloorMap";
+import TimeTableAndMeal from "./pages/TimeTableAndMeal";
+import Setting from "./pages/setting";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <ExamAnd />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashBoard />}></Route>
+          <Route path="/ExamAnd" element={<ExamAnd />}></Route>
+          <Route path="/SchoolSchedule" element={<SchoolSchedule />}></Route>
+          <Route path="/FloorMap" element={<FloorMap />}></Route>
+          <Route
+            path="/TimeTableAndMeal"
+            element={<TimeTableAndMeal />}
+          ></Route>
+          <Route path="/Setting" element={<Setting />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
